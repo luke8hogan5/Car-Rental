eb575f2699357c43152b297bd561e06c7c3ce67a
NarrowOne <comkilbane@gmail.com>
2019-11-07 18:18:16 +0000

    Updated constructors & shit

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -4,14 +4,21 @@ public class Customer extends UserModel {
     private int custID;
     private double balance;
     private int loyaltyPts;
+    private String address;
 
     public Customer(){
+        super("John Doe", "", "john.doe@rand.com");
+        custID = 1;
+        balance = 1000.00;
+        loyaltyPts = 50;
+        address = "Somewhere, Somewhere";
     }
-    public Customer(String name, String password, int custID, int loyaltyPts, double balance) {
-        super(name, password);
+    public Customer(String name, String password, String email, int custID, int loyaltyPts, double balance, String address) {
+        super(name, password, email);
         this.custID = custID;
         this.balance = balance;
         this.loyaltyPts = loyaltyPts;
+        this.address = address;
     }
 
     public int getCustID() {
@@ -36,5 +43,19 @@ public class Customer extends UserModel {
 
     public void setLoyaltyPts(int loyaltyPts) {
         this.loyaltyPts = loyaltyPts;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLoyaltyTier(){
+        String tier = "Scrub";
+
+        return tier;
     }
 }
--- src/models/UserModel.java
+++ src/models/UserModel.java
@@ -2,23 +2,42 @@ package models;
 public class UserModel {
 	private String name;
 	private String password;
+	private String email;
 
 	public UserModel(){}
+
 	public UserModel(String name, String password) {
 		this.name = name;
 		this.password = password;
+	}
+
+	public UserModel(String name, String password, String email) {
+		this.name = name;
+		this.password = password;
+		this.email = email;
 	}
 	
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
