80a44134dc22fd79835aba6b92445e81f6fe9a27
xuanzhougrace_ <15117545@studentmail.ul.ie>
2019-11-08 17:21:14 +0000

    Catalog View folder

---
+++ src/CatalogView/CatalogView1.java
@@ -0,0 +1,101 @@
+package views;
+
+import javax.swing.*;
+import javax.swing.table.JTableHeader;
+ 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+import database.Database;
+
+public class CatalogView1 extends JFrame {
+	
+	private JScrollPane scpDemo;
+	private JTableHeader jth;
+	private JTable tabDemo;
+	private JButton btnShow;
+	
+	public CatalogView1(){
+		super("Catalog view");
+		initWindow();
+	}
+		
+	public void initWindow() {
+
+		this.setSize(330,400);
+		this.setLayout(null);
+		this.setLocation(100,50);
+		
+		this.scpDemo = new JScrollPane();
+		this.scpDemo.setBounds(10,50,300,270);
+		this.btnShow = new JButton("Display Catalog");
+		this.btnShow.setBounds(10,10,300,30);
+		
+		this.btnShow.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent ae){
+		btnShow_ActionPerformed(ae);
+		}
+		});
+		
+		add(this.scpDemo);
+		add(this.btnShow);
+		
+		this.setVisible(true);
+		}
+		
+	
+	
+		public void btnShow_ActionPerformed(ActionEvent ae){
+		
+		try{
+			
+			
+		Connection conn = Database.getConnection();
+		String sql = "SELECT vehicleMake,vehicleModel,vehiclePrice,vehicleYear FROM vehicle;";
+		//vehicleMake, vehiclePrice,vehicelModel,vehicleYear
+		PreparedStatement st = conn.prepareStatement(sql);
+		//Statement st = conn.createStatement(sql);
+	    ResultSet rs = st.executeQuery();	
+									
+		int count = 0;
+		while(rs.next()){
+			System.out.println(count);
+		count++;
+		}
+		rs = st.executeQuery();
+		
+		Object[][] info = new Object[count][4];//4
+		count = 0;
+		while(rs.next()){
+		info[count][0] = rs.getString("vehicleMake");
+
+		info[count][1] = rs.getString("vehicleModel");
+		info[count][2] = Double.valueOf( rs.getDouble("vehiclePrice") );
+		info[count][3] = Integer.valueOf(rs.getInt("vehicleYear"));
+		count++;
+		//System.out.println(rs.getString("vehicleMake"));
+		}
+		
+		
+		String[] title = {"VehicleMake","vehicleModel","vehiclePrice","vehicleYear"};
+		
+		this.tabDemo = new JTable(info,title);
+		
+		this.jth = this.tabDemo.getTableHeader();
+		
+		this.scpDemo.getViewport().add(tabDemo); 
+		
+		
+
+		
+		
+		}catch(SQLException sqle){
+		JOptionPane.showMessageDialog(null,"Data Error","Error",JOptionPane.ERROR_MESSAGE);
+		}
+		}
+}
+
+
+
+
---
+++ src/CatalogView/Main.java
@@ -0,0 +1,68 @@
+import java.sql.SQLException;
+
+import javax.swing.SwingUtilities;
+
+//import controllers.LoginController;
+//import controllers.RegisterController;
+import database.Database;
+//import views.LoginView;
+//import views.MasterView;
+//import views.RegisterView;
+import views.CatalogView1;
+import controllers.CatalogController;
+//import 
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				Database db = null;
+				try {
+					db = Database.getInstance();
+				} catch (SQLException e1) {
+					System.out.print("Database not initialised");
+					e1.printStackTrace();
+				}
+				if(db != null) {
+					//initLogin();
+					//initRegister();
+					initCatalog();
+					
+				}
+				
+				try {
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+/*	public static void initLogin() {	
+		LoginView logView = new LoginView();
+		LoginController logCtrl = new LoginController(logView);
+			
+		logView.setLoginListener(logCtrl);
+	}
+		
+		public static void initRegister() {
+			RegisterView regView = new RegisterView();
+			RegisterController regCtrl = new RegisterController(regView);
+			
+			regView.setRegisterListener(regCtrl);
+		}
+}*/
+	public static void initCatalog() {	
+		CatalogView1 CatalogV = new CatalogView1();
+		CatalogV.initWindow(); 
+		//CatalogController CatalogCtrl = new CatalogController(CatalogV);
+		
+		//Catalog.setCatalogListener(CatalogCtrl);
+	}
+}
+
+
