9064c54d7c3efcdbc974a6e66b3fabc2be2e8191
16182359 <16182359@studentmail.ul.ie>
2019-11-12 14:01:29 +0000

    dao

---
+++ src/dao/UserDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.util.List;
+
+import models.UserModel;
+
+public interface UserDao {
+
+	UserModel getUser();
+	List<UserModel> getAllUsers();
+	UserModel getUserByUserNameAndPassword(String username, String password);
+	void insertUser(UserModel user);
+	void updateUser(UserModel user);
+	void deleteUser(int userId);
+	
+}
